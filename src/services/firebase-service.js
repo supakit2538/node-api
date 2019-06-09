6ebf855f47db15af4f3184f48f86f788d8a069c1
supakit <supakitzaaa@gmail.com>
2019-06-09 17:49:52 +0700

    test felx message

--- src/services/firebase-service.js
+++ src/services/firebase-service.js
@@ -1,6 +1,6 @@
 const firebase = require("firebase-admin");
 const serviceAccount = require("../../linebot-43484-firebase-adminsdk-eyv4h-3d2476ffec.json");
-var db, ref,refCont;
+var db, ref, refCont;
 
 class FirebaseService {
   constructor() {
@@ -21,7 +21,63 @@ class FirebaseService {
         return ref.once("value", function(snapshot) {
           let _order = snapshot.val();
 
-          return resolve(JSON.stringify(_order));
+          msg = {
+            type: "template",
+            altText: "this is a carousel template",
+            template: {
+              type: "carousel",
+              columns: [
+                {
+                  thumbnailImageUrl:
+                    "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
+                  title: "this is menu",
+                  text: "description",
+                  actions: [
+                    {
+                      type: "postback",
+                      label: "Buy",
+                      data: "action=buy&itemid=111"
+                    },
+                    {
+                      type: "postback",
+                      label: "Add to cart",
+                      data: "action=add&itemid=111"
+                    },
+                    {
+                      type: "uri",
+                      label: "View detail",
+                      uri: "http://example.com/page/111"
+                    }
+                  ]
+                },
+                {
+                  thumbnailImageUrl:
+                    "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
+                  title: "this is menu",
+                  text: "description",
+                  actions: [
+                    {
+                      type: "postback",
+                      label: "Buy",
+                      data: "action=buy&itemid=222"
+                    },
+                    {
+                      type: "postback",
+                      label: "Add to cart",
+                      data: "action=add&itemid=222"
+                    },
+                    {
+                      type: "uri",
+                      label: "View detail",
+                      uri: "http://example.com/page/222"
+                    }
+                  ]
+                }
+              ]
+            }
+          };
+
+          return resolve(JSON.stringify(msg));
         });
       } catch (e) {
         return reject(e);
